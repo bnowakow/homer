4c9e44e132e25b474c127ecf6c213f5729a97719
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2023-03-05 16:31:40 +0100

    try update config

---
+++ copy-from-container.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo cp /var/lib/kubelet/pods/8427bb1a-bd4e-4b96-af60-3ce7a91af8a8/volumes/kubernetes.io~csi/pvc-2962328c-1fb7-4db5-8f7c-3e3503ff0499/mount/config.yml /mnt/MargokPool/home/sup/code/homer/src/assets/defaults.yml
+
