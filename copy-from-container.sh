9e198bae9b0997b57eef843f5534e82c562efa22
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2023-03-05 16:57:03 +0100

    add icons

--- copy-from-container.sh
+++ copy-from-container.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 sudo cp /var/lib/kubelet/pods/8427bb1a-bd4e-4b96-af60-3ce7a91af8a8/volumes/kubernetes.io~csi/pvc-2962328c-1fb7-4db5-8f7c-3e3503ff0499/mount/config.yml /mnt/MargokPool/home/sup/code/homer/src/assets/defaults.yml
+sudo cp -r /var/lib/kubelet/pods/8427bb1a-bd4e-4b96-af60-3ce7a91af8a8/volumes/kubernetes.io~csi/pvc-2962328c-1fb7-4db5-8f7c-3e3503ff0499/mount/tools/ /mnt/MargokPool/home/sup/code/homer/public/assets/
 
