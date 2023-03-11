b34862cc05cbe84dbec308eff63e85091cc62b65
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2023-03-11 11:56:43 +0100

    add remote plex. fix copy-from-container.sh since pvc dir is moving

--- copy-from-container.sh
+++ copy-from-container.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-sudo cp /var/lib/kubelet/pods/8427bb1a-bd4e-4b96-af60-3ce7a91af8a8/volumes/kubernetes.io~csi/pvc-2962328c-1fb7-4db5-8f7c-3e3503ff0499/mount/config.yml /mnt/MargokPool/home/sup/code/homer/src/assets/defaults.yml
-sudo cp -r /var/lib/kubelet/pods/8427bb1a-bd4e-4b96-af60-3ce7a91af8a8/volumes/kubernetes.io~csi/pvc-2962328c-1fb7-4db5-8f7c-3e3503ff0499/mount/tools/ /mnt/MargokPool/home/sup/code/homer/public/assets/
+# TODO throw error when found more than 1 dir
+homer_pvc_dir=$(sudo find /var/lib/kubelet/pods/ -name config.yml | sed 's/\/config.yml//')
+
+
+sudo cp $homer_pvc_dir/config.yml /mnt/MargokPool/home/sup/code/homer/src/assets/defaults.yml
+sudo cp -r $homer_pvc_dir/tools/ /mnt/MargokPool/home/sup/code/homer/public/assets/
 
