78d0a3d9205304abe5c305ee23d96e22603d4d1f
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2023-04-11 17:37:14 +0200

    error check in copy-pvc-lib.sh. change zabbix url.

--- copy-pvc-lib.sh
+++ copy-pvc-lib.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ $(sudo find /var/lib/kubelet/pods/ -name config.yml.dist | sed 's/\/config.yml.dist//' | wc -l) -gt 1 ]; then
+    echo "found more than one dir in ./copy-pvc-lib.sh" >&2
+    exit
+fi
+
 # TODO throw error when found more than 1 dir
-homer_pvc_dir=$(sudo find /var/lib/kubelet/pods/ -name config.yml | sed 's/\/config.yml//')
+homer_pvc_dir=$(sudo find /var/lib/kubelet/pods/ -name config.yml.dist | sed 's/\/config.yml.dist//')
 
--- copy-to-container.sh
+++ copy-to-container.sh
@@ -2,9 +2,6 @@
 
 source copy-pvc-lib.sh
 
-$homer_pvc_dir
-
-
-sudo cp /mnt/MargokPool/home/sup/code/homer/src/assets/defaults.yml /var/lib/kubelet/pods/8427bb1a-bd4e-4b96-af60-3ce7a91af8a8/volumes/kubernetes.io~csi/pvc-2962328c-1fb7-4db5-8f7c-3e3503ff0499/mount/config.yml
-sudo cp -r /mnt/MargokPool/home/sup/code/homer/public/assets/tools /var/lib/kubelet/pods/8427bb1a-bd4e-4b96-af60-3ce7a91af8a8/volumes/kubernetes.io~csi/pvc-2962328c-1fb7-4db5-8f7c-3e3503ff0499/mount
+sudo cp /mnt/MargokPool/home/sup/code/homer/src/assets/defaults.yml $homer_pvc_dir/config.yml
+sudo cp -r /mnt/MargokPool/home/sup/code/homer/public/assets/tools $homer_pvc_dir
 
