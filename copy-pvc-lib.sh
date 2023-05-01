b50e78c40957d638b787b5983ae4fcecf1aed876
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2023-05-01 13:37:56 +0200

    add jellyfin. icon from https://icons-for-free.com/jellyfin-1331550889904990605/

--- copy-pvc-lib.sh
+++ copy-pvc-lib.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# TODO check if zero results were found and throw error
 if [ $(sudo find /var/lib/kubelet/pods/ -name config.yml.dist | sed 's/\/config.yml.dist//' | wc -l) -gt 1 ]; then
     echo "found more than one dir in ./copy-pvc-lib.sh" >&2
     exit
@@ -7,4 +8,5 @@ fi
 
 # TODO throw error when found more than 1 dir
 homer_pvc_dir=$(sudo find /var/lib/kubelet/pods/ -name config.yml.dist | sed 's/\/config.yml.dist//')
+echo "homer_pvc_dir=$homer_pvc_dir path found"
 
