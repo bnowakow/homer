7ac2861fc5225d1d931d89057eac2292edd8f573
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2023-05-29 19:16:08 +0200

    add youtube-dl. icon from https://iconduck.com/icons/112484/youtube-dl

--- copy-pvc-lib.sh
+++ copy-pvc-lib.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # TODO check if zero results were found and throw error
-if [ $(sudo find /var/lib/kubelet/pods/ -name config.yml.dist | sed 's/\/config.yml.dist//' | wc -l) -gt 1 ]; then
+if [ $(sudo find /var/lib/kubelet/pods/ -name config.yml.dist 2>/dev/null | sed 's/\/config.yml.dist//' | wc -l) -gt 1 ]; then
     echo "found more than one dir in ./copy-pvc-lib.sh" >&2
     exit
 fi
 
 # TODO throw error when found more than 1 dir
-homer_pvc_dir=$(sudo find /var/lib/kubelet/pods/ -name config.yml.dist | sed 's/\/config.yml.dist//')
+homer_pvc_dir=$(sudo find /var/lib/kubelet/pods/ -name config.yml.dist 2>/dev/null | sed 's/\/config.yml.dist//')
 echo "homer_pvc_dir=$homer_pvc_dir path found"
 
